39e545deb31576025d7d22bd8f38b1ead556c7d8
Ron Wieder <ron.wieder@tomiaglobal.com>
2019-12-24 11:54:36 +0200

    Conditional Logic, Looping and Arrays -> Demo: CalcEngine with Arrays, Loop, and Switch

--- src/main/java/com/ronplayground/calculator/Main.java
+++ src/main/java/com/ronplayground/calculator/Main.java
@@ -3,23 +3,32 @@ package com.ronplayground.calculator;
 public class Main {
 
     public static void main(String[] args) {
-	double val1 = 50.0d;
-	double val2 = 25.0d;
-	double result;
-	char opCode = 'a';
+	double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+	double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+	char[] opCodes = {'d', 'a', 's', 'm'};
+	double[] results = new double[opCodes.length];
 
-	if(opCode == 'a')
-		result = val1 + val2;
-	else if(opCode == 'a')
-		result = val1 - val2;
-	else if(opCode == 'a')
-		result = val1 / val2;
-	else if(opCode == 'm')
-		result = val1 * val2;
-	else {
-		System.out.println("Error - invalid shit");
-		result = 0.0d;
+	for(int i = 0; i < opCodes.length; i++) {
+		switch(opCodes[i]) {
+			case 'a':
+				results[i] = leftVals[i] + rightVals[i];
+				break;
+			case 's':
+				results[i] = leftVals[i] - rightVals[i];
+				break;
+			case 'd':
+				results[i] = leftVals[i] / rightVals[i];
+				break;
+			case 'm':
+				results[i] = leftVals[i] * rightVals[i];
+				break;
+			default:
+				System.out.println("Error - invalid shit");
+				results[i] = 0.0d;
+				break;
+		}
 	}
-	    System.out.println(result);
+	for(double result:results)
+	    System.out.println("result = " + result);
     }
 }

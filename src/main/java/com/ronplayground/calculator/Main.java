f80a803d9a9691b251252cccf00c87369eba03ef
Ron Wieder <ron.wieder@tomiaglobal.com>
2019-12-24 11:31:23 +0200

    Variables, Data Types and Math Operators -> Demo: Type conversion

--- src/main/java/com/ronplayground/calculator/Main.java
+++ src/main/java/com/ronplayground/calculator/Main.java
@@ -3,6 +3,16 @@ package com.ronplayground.calculator;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	float floatVal = 1.0f;
+	double doubleVal = 4.0d;
+	byte byteVal = 7;
+	short shortVal = 7;
+	long longVal = 5;
+
+	short result1 = byteVal;
+	short result2 = (short)longVal;
+	long result3 = shortVal;
+
+        System.out.println("Success");
     }
 }

1d13471936fdfa302b69740113d407948ab5f470
Ron Wieder <ron.wieder@tomiaglobal.com>
2019-12-24 14:29:25 +0200

    Class Initializers and Constructors -> Demo: CalcEngine with Field Initializers and Constructors

--- src/main/java/com/ronplayground/calculator/Main.java
+++ src/main/java/com/ronplayground/calculator/Main.java
@@ -8,11 +8,15 @@ public class Main {
 //	char[] opCodes = {'d', 'a', 's', 'm'};
 //	double[] results = new double[opCodes.length];
 
+//	    MathEquation testEquation = new MathEquation();
+//	    testEquation.execute();
+//	    System.out.println("result = " + testEquation.getResult());
+
 	    MathEquation[] equations = new MathEquation[4];
-	    equations[0] = create(100.0d, 50.0d, 'd');
-	    equations[1] = create(25.0d, 92.0d, 'a');
-	    equations[2] = create(225.0d, 17.0d, 's');
-	    equations[3] = create(11.0d, 3.0d, 'm');
+	    equations[0] = new MathEquation(100.0d, 50.0d, 'd');
+	    equations[1] = new MathEquation(25.0d, 92.0d, 'a');
+	    equations[2] = new MathEquation(225.0d, 17.0d, 's');
+	    equations[3] = new MathEquation(11.0d, 3.0d, 'm');
 
 	    for(MathEquation equation:equations) {
 			    equation.execute();
@@ -20,12 +24,12 @@ public class Main {
 	    }
     }
 
-    public static MathEquation create(double leftVal, double rightVal, char opCode){
-    	MathEquation equation = new MathEquation();
-    	equation.setLeftVal(leftVal);
-    	equation.setRightVal(rightVal);
-    	equation.setOpCode(opCode);
-
-    	return equation;
-    };
+//    public static MathEquation create(double leftVal, double rightVal, char opCode){
+//    	MathEquation equation = new MathEquation();
+//    	equation.setLeftVal(leftVal);
+//    	equation.setRightVal(rightVal);
+//    	equation.setOpCode(opCode);
+//
+//    	return equation;
+//    };
 }
--- src/main/java/com/ronplayground/calculator/MathEquation.java
+++ src/main/java/com/ronplayground/calculator/MathEquation.java
@@ -11,7 +11,20 @@ public class MathEquation {
 	@Setter @Getter private double leftVal;
 	@Setter @Getter private double rightVal;
 	@Getter private double result;
-	@Setter @Getter private char opCode;
+	@Setter @Getter private char opCode = 'a';
+
+	public MathEquation() {
+	}
+
+	public MathEquation(char opCode) {
+		this.opCode = opCode;
+	}
+
+	public MathEquation(double leftVal, double rightVal, char opCode) {
+		this(opCode);
+		this.rightVal = rightVal;
+		this.opCode = opCode;
+	}
 
 	public void execute(){
 		switch(opCode) {
